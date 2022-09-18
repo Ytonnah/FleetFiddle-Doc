85bb99ce9632b5cf8bb8274bf7317e4fbf7b0b24
Intona <62970031+Ytonnah@users.noreply.github.com>
2022-09-18 15:30:45 +0800

    added the basic layout pad of the window

--- UI.py
+++ UI.py
@@ -7,11 +7,14 @@ import random
 
 
 root = Tk()
-canvasSize = root.geometry('480x300')
-root.title('Sample CommandLine')
-root.resizable(False,False)
-
+width,height = root.winfo_screenwidth(),root.winfo_screenheight()
+canvasSize = root.geometry('%dx%d+0+0'%(width,height))
+root.title('WDocProj')
+root.config(background="#E4DCCF")#hex colors must have the hashtag in specifying
+#root.resizable(False,False)
+#root.attributes('-fullscreen',True)
 frm = ttk.Frame(root,padding=10)
+frm.config()
 frm.place()
 
 '''
@@ -20,7 +23,9 @@ test out core features. then move it on a separate library later
 
 
 #widgets goes here
-
+navFile1 = ttk.Button(text="File")
+navFile1.config()
+navFile1.place(width=100,height=25,x=10,y=10)
 
 
 root.mainloop()
