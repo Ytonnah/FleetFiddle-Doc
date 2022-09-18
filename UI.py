016d2397bdfdefe73b4c8c7d761177cf2a82a09b
Intona <62970031+Ytonnah@users.noreply.github.com>
2022-09-18 15:08:12 +0800

    Main widgets and utilities

---
+++ UI.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import ttk
+import keyboard as k
+from tkinter import Text
+
+import random
+
+
+root = Tk()
+canvasSize = root.geometry('480x300')
+root.title('Sample CommandLine')
+root.resizable(False,False)
+
+frm = ttk.Frame(root,padding=10)
+frm.place()
+
+'''
+test out core features. then move it on a separate library later
+'''
+
+
+#widgets goes here
+
+
+
+root.mainloop()
