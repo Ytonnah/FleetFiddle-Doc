263eaa52e03001372b77a370f26b313af303f1ab
Durid <62970031+Ytonnah@users.noreply.github.com>
2022-09-24 01:32:41 +0800

    fixed formatting appearance
    -placeholder text
    -pop up alignment
    -colors

--- UI.py
+++ UI.py
@@ -28,10 +28,7 @@ test out core features. then move it on a separate library later
 class liner:
     linecounter = 0#linecounted should be part of the method
     def addCodeLine(event):
-    
         linecounter+=1
-        #print("beep")
-        #a =  str(linecounter) + '\t\n'
         a = "something \n"
         textpad.insert(END,a)
         
@@ -39,8 +36,8 @@ class liner:
 
 class TopWidgets:
     def show():
-        edit.place(x= 100,y=26,width=50,height=100)#reseting the plcement
-        hidden = False
+        edit.place(x= 90,y=28,width=75,height=300)#reseting the plcement
+        
         navFile2.config(command=TopWidgets.hide)
     def hide():
         edit.place_forget()
@@ -54,7 +51,7 @@ def maincommand(a):
 #other window aspect
 canvas = Canvas(root,width=10000,height=45,background="#576F72",highlightthickness=0,bd=0,relief="ridge")
 canvas.place(x=0,y=0)
-canvas2 = Canvas(root,width=10000,height=45,background="#3D8361",highlightthickness=0,bd=0,relief="ridge")
+canvas2 = Canvas(root,width=10000,height=60,background="#3D8361",highlightthickness=0,bd=0,relief="ridge")
 canvas2.place(x=0,y=45)
 
 
@@ -73,7 +70,7 @@ navFile1.place(width=75,height=23,x=10,y=6)
 
 navFile2 = Button(text="Edit",style='Custom.TButton')
 navFile2.config (command=TopWidgets.show)
-navFile2.place(width=75,height=23,x=100,y=6)
+navFile2.place(width=75,height=23,x=90,y=6)
 #navFile2.bind('<Button-1>',TopWidgets.openFileBtn)
 #word editing utilities goes here.
 
@@ -86,15 +83,19 @@ edit.place_forget()
 
 #Add the content tools bar(below the nav bars)
 
+
 #1. the font alignment buttons (left,centered, right)
+alignment_y = 66 #base y alignment
+alignment_label = Label(text="Alignment:").place(x=845,y=(alignment_y - 20))
+
 L_allignment = Button(text='Left') #maybe replace it with icons later on
-L_allignment.place(x=800,y=50,width=50,height=20)
+L_allignment.place(x=800,y=alignment_y,width=50,height=20)
 
 C_allignment = Button(text='Cntr') #maybe replace it with icons later on
-C_allignment.place(x=850,y=50,width=50,height=20)
+C_allignment.place(x=850,y=alignment_y,width=50,height=20)
 
 R_allignment = Button(text='Rght') #maybe replace it with icons later on
-R_allignment.place(x=900,y=50,width=50,height=20)
+R_allignment.place(x=900,y=alignment_y,width=50,height=20)
 
 
 #2. the font Size field and fixed size buttons
@@ -122,10 +123,10 @@ def deletePlaceholderText(event):#functions that deletes the placeholder text(WO
         
 
 #text field goes here
-placeholderText1 = "/Type something here/"
+placeholderText1 = "Type something here"
 textpad = Text(bd=0)
 #textpad.insert(END,"0\t")
-textpad.config(font=("calibri",12))
+textpad.config(font=("calibri",12),fg="#808080")
 textpad.place(x=350,y=200,width=800,height=1500)
 textpad.insert(END,placeholderText1)
 textpad.tag_add("plc","1.0","1.10") #marker
